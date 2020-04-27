8a29461c600ed94613be52eaf4da99b17d49541d
Melissa Magaña <melissamagana@melissas-mbp.attlocal.net>
2020-04-27 18:24:49 -0500

    Retrieves data from API

--- src/PlanetContainer/index.js
+++ src/PlanetContainer/index.js
@@ -1,6 +1,35 @@
 import React, {Component} from 'react'
 
 export default class PlanetContainer extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			planets: []
+		}
+	}
+	componentDidMount() {
+		this.retrievePlanetData()
+	}
+	retrievePlanetData = async () => {
+		try {
+			const url = process.env.REACT_APP_API_URL + '/api/v1/planets/'
+			console.log('about to fetch data from:')
+			console.log(url)
+
+			const planetResponse = await fetch(url)
+			console.log('Response from fetch call:')
+			console.log(planetResponse)
+
+			const planetJson = await planetResponse.json()
+			console.log('retrievePlanetData:')
+			console.log(planetJson)
+
+		} catch (err) {
+			console.error('Error retrieving planet data')
+			console.error(err)
+		}
+	}
 	render() {
 		return(
 			<div className="PlanetContainer">
