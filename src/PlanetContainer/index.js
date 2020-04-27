6cb4396c1a273b2fecb3b7a1fb65785d5cef1b9b
Melissa Magaña <melissamagana@melissas-mbp.attlocal.net>
2020-04-27 18:10:01 -0500

    Initialized React App with PlanetContainer

---
+++ src/PlanetContainer/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class PlanetContainer extends Component {
+	render() {
+		return(
+			<div className="PlanetContainer">
+				<h2>Planet Container</h2>
+			</div>
+		)
+	}
+}
